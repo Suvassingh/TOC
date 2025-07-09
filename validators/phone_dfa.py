bf602958a257d49580f6db76ae80dca95316a901
Subhash Singh <ss5138275@gmail.com>
2025-07-09 08:17:39 +0545

    code

--- validators/phone_dfa.py
+++ validators/phone_dfa.py
@@ -6,4 +6,5 @@ def is_valid_phone(input_str):
             digits += 1
         elif char not in [' ', '-', '(', ')', '+']:
             return False
-    return 10 <= digits <= 15 and (input_str.startswith('+') or input_str[0].isdigit())
+    return 10 <= digits <= 15 
+# and (input_str.startswith('+') or input_str[0].isdigit())
